165d5dbe2b36b23dbe162bf44340fe74b921e4a2
Johan 'Horv' Andersson <drhorv@gmail.com>
2015-12-21 01:34:34 +0100

    Revert "Changed concat $ map to concatMap"
    This reverts commit 2e64b9e03236b75a36b00b5aad69fa07cfc9b99f.

--- day10/day10.hs
+++ day10/day10.hs
@@ -11,7 +11,7 @@ lookAndSay 0 s = s
 lookAndSay n s = lookAndSay (n-1) $ f' s
 
 f' :: String -> String
-f' s = concatMap m gs
+f' s = concat $ map m gs
     where
         gs = group s
         m :: String -> String
